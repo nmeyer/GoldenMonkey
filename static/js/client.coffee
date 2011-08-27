1021da74744c782361ea9b7e90cc1980b34dd0dd
Derek Dahmer <derekdahmer@gmail.com>
2011-08-27 16:29:02 -0400

    Rendering some sweet ass snakes

--- static/js/client.coffee
+++ static/js/client.coffee
@@ -2,10 +2,12 @@ $ ->
     width = 20
     height = 10
 
-    for x in [1..height]
+    for y in [1..height]
         row = $("#row-template").clone()
-        for y in [1..width]
+        for x in [1..width]
             box = $("#snake-box-template").clone()
+            box.attr('x',x)
+            box.attr('y',y)
             box.removeAttr('id')
             box.show()
             box.appendTo(row)
@@ -14,7 +16,10 @@ $ ->
         row.show()
         $(".game-board").append(row)
 
-
+    sampleCoords = [[3,3],[4,3],[5,3]]
+    sampleCoords2 = [[3,2],[3,3],[4,3]]
+    onGameState(sampleCoords)
+    setTimeout (-> onGameState(sampleCoords2)),1000
     # Start socket.io
     #socket = io.connect()
     #io.sockets.on 'connection', (socket) ->
@@ -22,3 +27,20 @@ $ ->
 
     #socket.on 'news', (data) ->
     #    console.log data
+
+
+
+boxAt = (x,y) ->
+    return $("[x=#{x}][y=#{y}]")
+
+onGameState = (coords) ->
+    # Clear all boxes
+    $(".snake-box").removeClass('enemy').removeClass('you')
+
+    # Use grid coordinates to highlight boxes
+    _.each coords, (coord) ->
+        [x,y] = coord
+        box = boxAt(x,y)
+        box.addClass('enemy')
+
+
--- static/js/client.js
+++ static/js/client.js
@@ -1,21 +1,40 @@
 (function() {
+  var boxAt, onGameState;
   $(function() {
-    var box, height, row, width, x, y, _results;
+    var box, height, row, sampleCoords, sampleCoords2, width, x, y;
     width = 20;
     height = 10;
-    _results = [];
-    for (x = 1; 1 <= height ? x <= height : x >= height; 1 <= height ? x++ : x--) {
+    for (y = 1; 1 <= height ? y <= height : y >= height; 1 <= height ? y++ : y--) {
       row = $("#row-template").clone();
-      for (y = 1; 1 <= width ? y <= width : y >= width; 1 <= width ? y++ : y--) {
+      for (x = 1; 1 <= width ? x <= width : x >= width; 1 <= width ? x++ : x--) {
         box = $("#snake-box-template").clone();
+        box.attr('x', x);
+        box.attr('y', y);
         box.removeAttr('id');
         box.show();
         box.appendTo(row);
       }
       row.removeAttr('id');
       row.show();
-      _results.push($(".game-board").append(row));
+      $(".game-board").append(row);
     }
-    return _results;
+    sampleCoords = [[3, 3], [4, 3], [5, 3]];
+    sampleCoords2 = [[3, 2], [3, 3], [4, 3]];
+    onGameState(sampleCoords);
+    return setTimeout((function() {
+      return onGameState(sampleCoords2);
+    }), 1000);
   });
+  boxAt = function(x, y) {
+    return $("[x=" + x + "][y=" + y + "]");
+  };
+  onGameState = function(coords) {
+    $(".snake-box").removeClass('enemy').removeClass('you');
+    return _.each(coords, function(coord) {
+      var box, x, y;
+      x = coord[0], y = coord[1];
+      box = boxAt(x, y);
+      return box.addClass('enemy');
+    });
+  };
 }).call(this);
