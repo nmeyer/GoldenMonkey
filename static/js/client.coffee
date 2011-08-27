3b664b2309d5b3ea802c158adf7db27dfe89ab9f
Derek Dahmer <derekdahmer@gmail.com>
2011-08-27 15:48:51 -0400

    Rendering a grid

--- static/js/client.coffee
+++ static/js/client.coffee
@@ -1,3 +1,16 @@
 $ ->
-    console.log 'loaded'
+    width = 20
+    height = 10
+
+    for x in [1..height]
+        row = $("#row-template").clone()
+        for y in [1..width]
+            box = $("#snake-box-template").clone()
+            box.removeAttr('id')
+            box.show()
+            box.appendTo(row)
+
+        row.removeAttr('id')
+        row.show()
+        $(".game-board").append(row)
 
--- static/js/client.js
+++ static/js/client.js
@@ -1,5 +1,21 @@
 (function() {
   $(function() {
-    return console.log('loaded');
+    var box, height, row, width, x, y, _results;
+    width = 20;
+    height = 10;
+    _results = [];
+    for (x = 1; 1 <= height ? x <= height : x >= height; 1 <= height ? x++ : x--) {
+      row = $("#row-template").clone();
+      for (y = 1; 1 <= width ? y <= width : y >= width; 1 <= width ? y++ : y--) {
+        box = $("#snake-box-template").clone();
+        box.removeAttr('id');
+        box.show();
+        box.appendTo(row);
+      }
+      row.removeAttr('id');
+      row.show();
+      _results.push($(".game-board").append(row));
+    }
+    return _results;
   });
 }).call(this);
