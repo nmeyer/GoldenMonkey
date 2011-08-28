2919ff13a75790109dbcac9dec19aa38516e27d6
Nick Meyer <nick@reptar.org>
2011-08-27 23:39:59 -0400

    added config module

---
+++ config.coffee
@@ -0,0 +1,7 @@
+@FPS = 3
+@SPEED = 1000 / @FPS
+
+@snakes =
+    len: 8
+@board =
+    size: 10
---
+++ config.js
@@ -0,0 +1,10 @@
+(function() {
+  this.FPS = 3;
+  this.SPEED = 1000 / this.FPS;
+  this.snakes = {
+    len: 8
+  };
+  this.board = {
+    size: 10
+  };
+}).call(this);
